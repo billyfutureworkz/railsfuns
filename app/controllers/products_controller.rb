04b76069fdb8807e7a958dff9f751d625642c6ce
hoasung01 <nguyenngochai.shipagent@gmail.com>
2015-07-08 08:37:40 +0700

    added show

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,9 @@
 class ProductsController < ApplicationController
+	def index
+		@products = Product.all.includes(:category).published
+	end
+
+	def show
+		@product = Product.find(params[:id])
+	end
 end
